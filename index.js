3b8ab0e6dcb164075a48c9a85467c60be162cbd5
QudratUllahKhan04 <qudratkhan.creator@gmail.com>
2025-08-22 01:40:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'https://e-certificates.bureauveratas.com', // or http://localhost:3000
+  origin: 'https://qrcode-chi-three.vercel.app', // or http://localhost:3000
   methods: ['GET'],
   credentials: true,
 };
